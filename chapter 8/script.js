d02c8599b0f0dbd8f74ba1d85f37d44f1c1d4b86
MUhammad Wahaj <mwahajjutt123@gmail.com>
2024-08-03 00:33:22 +0500

    Initial commit

---
+++ chapter 8/script.js
@@ -0,0 +1,16 @@
+
+<script>
+       myimage.style.widht='146px';
+       
+    btn.addEventListener("click",()=>{
+        if(myimage.style.widht==='546px'){
+
+            myimage.style.widht="146px";
+        }
+        else if(myimage.style.widht==='146px'){
+
+            myimage.style.widht="546px";
+        }
+       
+        })
+        </script>
